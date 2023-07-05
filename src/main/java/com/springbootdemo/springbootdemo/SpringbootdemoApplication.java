bef52b4456a693d92cb76272cc1ecafe2280ebda
Harish <r.shivi@yahoo.com>
2023-07-05 17:08:57 +0530

    feat: add custom autoconfiguration

--- src/main/java/com/springbootdemo/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/springbootdemo/springbootdemo/SpringbootdemoApplication.java
@@ -1,10 +1,21 @@
 package com.springbootdemo.springbootdemo;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SpringbootdemoApplication {
+
+	@Bean
+	public InitializingBean runner(DataSource dataSource) {
+		return () -> {
+			System.out.println("This is the datasource this Spring Boot project is using: " + dataSource);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
